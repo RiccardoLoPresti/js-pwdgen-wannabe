c3888512819089d3aea6d8a11924f11dbc3ab0bb
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-10 14:18:40 +0200

    [add] script

--- js/script.js
+++ js/script.js
@@ -4,4 +4,19 @@ let colorePreferito;
 let output;
 
 
+nomeUtente = prompt('Qualè il tuo nome?');
+
+cognomeUtente = prompt(`${nomeUtente} qualè il tuo cognome?`);
+
+nomeCompleto = nomeUtente + cognomeUtente;
+
+colorePreferito = prompt(`${nomeCompleto} qualè il tuo colore preferito?`);
+
+output = nomeCompleto + colorePreferito + 22;
+
+document.getElementById('password').innerHTML = `
+  La tua password sicurissima è:<span class="bg-info pt-3">${output} </span>
+`;
+
+
 
